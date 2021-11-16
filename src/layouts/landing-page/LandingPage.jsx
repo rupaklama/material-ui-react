1cfd7f680e0a2e5877a92db20bb29ed92283297b
pak <rupaklamadev@gmail.com>
2021-11-15 19:56:53 -0500

    added styling for landingpage

--- src/layouts/landing-page/LandingPage.jsx
+++ src/layouts/landing-page/LandingPage.jsx
@@ -1,9 +1,18 @@
+import { Button, Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Lottie from 'react-lottie';
 import animationData from '../../animations/landinganimation/data';
+import ButtonArrow from '../../components/button/ButtonArrow';
 
-const useStyles = makeStyles(theme => {});
+const useStyles = makeStyles(theme => ({
+  animation: {
+    maxWidth: '50em',
+    minWidth: '20em',
+    marginTop: '2em',
+    marginLeft: '10%',
+  },
+}));
 
 const LandingPage = () => {
   const classes = useStyles();
@@ -17,7 +26,34 @@ const LandingPage = () => {
     },
   };
 
-  return <Lottie options={defaultOptions} height={'100%'} width='100%' />;
+  return (
+    <Grid container direction='column'>
+      <Grid item>
+        <Grid container direction='row' justify='flex-end' alignItems='center'>
+          <Grid sm item>
+            <Typography variant='h2' align='center'>
+              By Savage Developers
+            </Typography>
+            <Grid container>
+              <Grid item>
+                <Button variant='contained'>Free Estimate</Button>
+              </Grid>
+              <Grid item>
+                <Button variant='outlined'>
+                  Learn more
+                  <ButtonArrow width={15} height={15} fill='red' />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid sm item className={classes.animation}>
+            <Lottie options={defaultOptions} height={'100%'} width='100%' />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default LandingPage;
--- src/shared/theme/Theme.js
+++ src/shared/theme/Theme.js
@@ -1,7 +1,7 @@
-import { createMuiTheme } from "@material-ui/core";
+import { createMuiTheme } from '@material-ui/core';
 
-const savageBlue = "#0B72B9";
-const savageOrange = "#ffba60";
+const savageBlue = '#0B72B9';
+const savageOrange = '#ffba60';
 
 // overriding default theme object
 export default createMuiTheme({
@@ -24,16 +24,23 @@ export default createMuiTheme({
   typography: {
     // defining our own global style for tab
     tab: {
-      fontFamily: "Railway",
-      textTransform: "none",
+      fontFamily: 'Railway',
+      textTransform: 'none',
       fontWeight: 700,
-      fontSize: "1rem",
+      fontSize: '1rem',
     },
     estimate: {
-      fontFamily: "Pacifico",
-      fontSize: "1rem",
-      textTransform: "none",
-      color: "#fff",
+      fontFamily: 'Pacifico',
+      fontSize: '1rem',
+      textTransform: 'none',
+      color: '#fff',
+    },
+    h2: {
+      fontFamily: 'Raleway',
+      fontWeight: 700,
+      fontSize: '2.5rem',
+      color: `${savageBlue}`,
+      lineHeight: 1.5,
     },
   },
 });
